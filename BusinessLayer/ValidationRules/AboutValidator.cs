c69662d4f91a1efa744ecc1b197a7c1b1af5320d
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-10-21 18:15:31 +0300

    Business katmanı oluşturuldu. Validation kuralları oluşturuldu. Front-end kısmı oluşturuldu. Partial viewlar oluşturuldu.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x=>x.Description).NotEmpty().WithMessage("Açıklamayı boş geçemezsiniz.");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Görsel Seçiniz!...");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Açıklama 50 karakterden az olamaz!...");
+            RuleFor(x => x.Description).MaximumLength(5000).WithMessage("Lütfen açıklamayı kısaltınız.");
+        }
+    }
+}
