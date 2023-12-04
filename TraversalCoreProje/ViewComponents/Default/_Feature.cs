48523df1b0971dde4e5e2d53af28802e208e3d4d
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-04 20:40:08 +0300

    subabout eklendi

--- TraversalCoreProje/ViewComponents/Default/_Feature.cs
+++ TraversalCoreProje/ViewComponents/Default/_Feature.cs
@@ -9,8 +9,9 @@ namespace TraversalCoreProje.ViewComponents.Default
         FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
        public IViewComponentResult Invoke()
         {
-            var values = featureManager.TGetList();
-            return View(values);
+            //var values = featureManager.TGetList();
+            //ViewBag.image1=featureManager.
+            return View();
         }
     }
 }
