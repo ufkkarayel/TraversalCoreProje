e3163b8227afadbe7399e6ced0de92ab9a1dc7cb
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-04 21:04:09 +0300

    UI katmanı tamamlandı

--- TraversalCoreProje/ViewComponents/Default/_Testimonial.cs
+++ TraversalCoreProje/ViewComponents/Default/_Testimonial.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.ViewComponents.Default
 {
     public class _Testimonial : ViewComponent
     {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = testimonialManager.TGetList();
+            return View(values);
         }
     }
 }
