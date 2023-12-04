8c9eedb0e00677d3f00d0790220ec06f9cf021c8
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-04 22:09:14 +0300

    tema düzenlendi

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
 {
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
-            return View();
+            var values = destinationManager.TGetList();
+            return View(values);
         }
     }
 }
