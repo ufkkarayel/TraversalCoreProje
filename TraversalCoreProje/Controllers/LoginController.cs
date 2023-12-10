60f45eb710fe49314bc230a60e81f2fdcff3c1f6
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-10 20:49:05 +0300

    profile controller düzenlendi

--- TraversalCoreProje/Controllers/LoginController.cs
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -66,7 +66,7 @@ namespace TraversalCoreProje.Controllers
                 var result=await _signInManager.PasswordSignInAsync(p.UserName,p.Password,false,true);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("Index", "Destination");
+                    return RedirectToAction("Index", "Profile",new {Area="Member" });
                 }
                 else
                 {
