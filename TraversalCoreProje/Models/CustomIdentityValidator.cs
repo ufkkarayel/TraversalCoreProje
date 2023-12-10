060d722653b11b8939cce3489d54effc737aacdf
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-10 15:51:43 +0300

    validation işlemleri yapıldı

---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TraversalCoreProje.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordTooShort",
+                Description = $"Parola minimum {length} karakter olmalıdır"
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola en az 1 sembol içermeli"
+
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parola en az 1 tane küçük harf içermeli"
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresUpper",
+                Description = "Parola en az 1 tane büyük harf içermeli"
+            };
+        }
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresDigit",
+                Description="Parola en az 1 adet rakam içermeli(0-9)"
+            };
+        }
+    }
+}
