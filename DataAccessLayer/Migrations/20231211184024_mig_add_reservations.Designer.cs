3b13c9099f48e0d362c1e57986d71a39e6a36f8e
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-11 22:42:17 +0300

    reservation sınıfı eklendi

--- DataAccessLayer/Migrations/20231211184024_mig_add_reservations.Designer.cs
+++ DataAccessLayer/Migrations/20231211184024_mig_add_reservations.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231211184024_mig_add_reservations")]
+    partial class mig_add_reservations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
