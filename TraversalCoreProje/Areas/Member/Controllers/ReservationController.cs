0b8c1af11775fd2c76716f9022bab4c4efe324bf
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-14 21:12:53 +0300

    viewlar eklendi

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -38,7 +38,7 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            p.AppUserID = 3;
+            p.AppUserID = 4;
             p.Status = "Onay Bekliyor";
             reservationManager.TAdd(p);
             return RedirectToAction("MyCurrentReservation");
