72bd086bcc266206cb4ec644021b4bd27b2215cf
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-14 20:27:09 +0300

    reservation işlemi için listeleme yapıldı

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -1,17 +1,29 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TraversalCoreProje.Areas.Member.Controllers
 {
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
         }
         public IActionResult MyOldReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value=x.DestinationID.ToString()
+                                           }).ToList();
             return View();
         }
 
