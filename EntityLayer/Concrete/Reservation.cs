0e4ff4121f21cabf7511bf509efb3558d4414d99
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-16 14:51:12 +0300

    destination ve rezervasyon tablolarına ilişki  verildi.

--- EntityLayer/Concrete/Reservation.cs
+++ EntityLayer/Concrete/Reservation.cs
@@ -16,6 +16,8 @@ namespace EntityLayer.Concrete
         public DateTime ReservationDate { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
-        
+        public int DestinationID { get; set; }
+        public Destination Destination { get; set; }
+
     }
 }
