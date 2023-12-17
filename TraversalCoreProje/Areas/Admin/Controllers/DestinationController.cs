3c561b2ea88f906f4d576505bf1038d9f3238cda
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-17 14:45:09 +0300

    crud metotları yapıldı

--- TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
@@ -12,6 +13,35 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
         {
             var values = destinationManager.TGetList();
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteDestination(int id)
+        {
+            var values = destinationManager.TGetByID(id);
+            destinationManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateDestination(int id) 
+        {
+            var values = destinationManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index");
         }
     }
 }
