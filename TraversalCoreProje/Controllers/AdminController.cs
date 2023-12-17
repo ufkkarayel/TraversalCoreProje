7d9eb9f3dbdbf7b8641bc9d55f9e540783344d76
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-17 15:34:48 +0300

    admin layout partiallara ayrıldı

---
+++ TraversalCoreProje/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+    }
+}
