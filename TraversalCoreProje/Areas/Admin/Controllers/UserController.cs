288f99d04c1296de74715fe3a7f643b49702f5b0
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-23 15:41:29 +0300

    listeleme işlemi yapıldı

--- TraversalCoreProje/Areas/Admin/Controllers/UserController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/UserController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class UserController : Controller
     {
+        private readonly IAppUserService _appUserService;
+
+        public UserController(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+           var values=_appUserService.TGetList();
+            return View(values);
         }
     }
 }
