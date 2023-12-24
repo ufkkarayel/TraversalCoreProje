607e9c4b75b2b3cddbe75016284a9d5db119bb89
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-24 22:26:07 +0300

    rehberler listelendi

---
+++ TraversalCoreProje/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+	public class GuideController : Controller
+	{
+		private readonly IGuideService _guideService;
+
+		public GuideController(IGuideService guideService)
+		{
+			_guideService = guideService;
+		}
+
+		public IActionResult Index()
+		{
+			var  values= _guideService.TGetList();
+			return View(values);
+		}
+	}
+}
