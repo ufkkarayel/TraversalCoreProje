6a1d251fe10291dcddf5de91423c619854c246c1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-09 21:26:04 +0300

    ...

--- TraversalCoreProje/Controllers/PdfReportController.cs
+++ TraversalCoreProje/Controllers/PdfReportController.cs
@@ -14,11 +14,15 @@ namespace TraversalCoreProje.Controllers
         public IActionResult StaticPdfReport()
         {
             string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PdfReports/" + "dosya1.pdf");
-            var stream = new FileStream(path,FileMode.Create);
+            var stream = new FileStream(path,FileMode.Create); 
+
             Document document = new Document(PageSize.A4);
             PdfWriter.GetInstance(document, stream);
-            document.Open();
+
+            document.Open(); 
+
             Paragraph paragraph = new Paragraph("Traversal Rezervasyon Pdf Raporu");
+
             document.Add(paragraph);
             document.Close();
             return File("/PdfReports/dosya1.pdf", "application/pdf", "dosya1.pdf");
