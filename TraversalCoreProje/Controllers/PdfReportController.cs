cf71cd7db98f9ab0026a072fcfceaa00bb9e421f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-09 21:33:16 +0300

    ..

--- TraversalCoreProje/Controllers/PdfReportController.cs
+++ TraversalCoreProje/Controllers/PdfReportController.cs
@@ -27,6 +27,38 @@ namespace TraversalCoreProje.Controllers
             document.Close();
             return File("/PdfReports/dosya1.pdf", "application/pdf", "dosya1.pdf");
         }
+        public IActionResult StaticCustomerReport()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/PdfReports/" + "dosya2.pdf");
+            var stream = new FileStream(path, FileMode.Create);
+
+            Document document = new Document(PageSize.A4);
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+
+            PdfPTable pdfPTable = new PdfPTable(3);
+            pdfPTable.AddCell("Misafir Adı");
+            pdfPTable.AddCell("Misafir Soyadı");
+            pdfPTable.AddCell("Misafir TC");
+
+            pdfPTable.AddCell("Ayse");
+            pdfPTable.AddCell("Öztürk");
+            pdfPTable.AddCell("1234567913");
+
+            pdfPTable.AddCell("Eylül");
+            pdfPTable.AddCell("Öztürk");
+            pdfPTable.AddCell("1234567912");
+
+            pdfPTable.AddCell("Eray");
+            pdfPTable.AddCell("Öztürk");
+            pdfPTable.AddCell("1234567955");
+
+            document.Add(pdfPTable);
+
+            document.Close();
+            return File("/PdfReports/dosya2.pdf", "application/pdf", "dosya2.pdf");
+        }
     }
 }
 
