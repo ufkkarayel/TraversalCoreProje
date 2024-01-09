8231466cfa63757e0da7c7c421917b3c590b2605
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-09 21:51:37 +0300

    excel raporları dinamik yapıldı

--- BusinessLayer/Concrete/ExcelManager.cs
+++ BusinessLayer/Concrete/ExcelManager.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class ExcelManager
+    public class ExcelManager : IExcelService
     {
+        public byte[] ExcelList<T>(List<T> t) where T : class
+        {
+
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
+            workSheet.Cells["A1"].LoadFromCollection(t,true,OfficeOpenXml.Table.TableStyles.Light10);
+
+            return excel.GetAsByteArray();
+        }
     }
 }
--- TraversalCoreProje/Controllers/ExcelController.cs
+++ TraversalCoreProje/Controllers/ExcelController.cs
@@ -1,4 +1,5 @@
-﻿using ClosedXML.Excel;
+﻿using BusinessLayer.Abstract;
+using ClosedXML.Excel;
 using DataAccessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
@@ -11,6 +12,13 @@ namespace TraversalCoreProje.Controllers
 {
     public class ExcelController : Controller
     {
+        private readonly IExcelService _excelService;
+
+        public ExcelController(IExcelService excelService)
+        {
+            _excelService = excelService;
+        }
+
         public IActionResult Index()
         {
            return View();
@@ -33,22 +41,9 @@ namespace TraversalCoreProje.Controllers
         }
         public IActionResult StaticExcelReport()
         {
-            ExcelPackage excel = new ExcelPackage();
-            var workSheet = excel.Workbook.Worksheets.Add("Sayfa1");
-            workSheet.Cells[1, 1].Value = "Rota";
-            workSheet.Cells[1, 2].Value = "Rehber";
-            workSheet.Cells[1, 3].Value = "Kontenjan";
+            return File(_excelService.ExcelList(DestinationList()),"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","YeniExcel.xlsx");
 
-            workSheet.Cells[2, 1].Value = "Gürcistan Turu";
-            workSheet.Cells[2, 2].Value = "Kadir Yıldız";
-            workSheet.Cells[2, 3].Value = "50";
-
-            workSheet.Cells[3, 1].Value = "Balkan Turu";
-            workSheet.Cells[3, 2].Value = "Zeynep Öztürk";
-            workSheet.Cells[3, 3].Value = "30";
-
-            var bytes = excel.GetAsByteArray();
-            return File(bytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "dosya.xlsx");
+            //application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
         }
         public IActionResult DestinationExcelReport()
         {
