9958952b89766f8f59f539aa7f7053d00a95be97
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-11 21:38:30 +0300

    mail controller oluşturuldu

--- TraversalCoreProje/Areas/Admin/Controllers/MailController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/MailController.cs
@@ -4,6 +4,12 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
 {
     public class MailController : Controller
     {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
         public IActionResult Index()
         {
             return View();
---
+++ TraversalCoreProje/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TraversalCoreProje.Models
+{
+    public class MailRequest
+    {
+        public string SenderMail { get; set; }
+        public string RecieverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
