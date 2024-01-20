dec281e370c400857cd4221f1af259427eafbefd
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-20 18:00:06 +0300

    ..

---
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
