3d262af7190fc0e38db18e051aa8df1ed44400a6
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2024-01-20 18:03:35 +0300

    .

--- TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
+++ TraversalCoreProje/Areas/Admin/Controllers/CityController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje.Areas.Admin.Controllers
 {
@@ -9,5 +12,27 @@ namespace TraversalCoreProje.Areas.Admin.Controllers
         {
             return View();
         }
+        public static List<CityClass> cities = new List<CityClass>()
+        {
+            new CityClass()
+            {
+                CityID = 1,
+                CityName = "Kayseri",
+                CityCountry="Türkiye"
+            },
+            new CityClass()
+            {
+                CityID= 2,
+                CityCountry="İngiltere",
+                CityName="Londra"
+
+            },
+            new CityClass()
+            {
+                CityID=3,
+                CityName="Roma",
+                CityCountry="İtalya"
+            }
+        };
     }
 }
